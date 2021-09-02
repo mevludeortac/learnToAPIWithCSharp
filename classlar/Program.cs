86d890e7f835abc5ac9bed5940d3543d74a7ea35
mevludeortac <ortacmevlude@gmail.com>
2021-09-02 15:49:52 +0300

    class-methods

---
+++ classlar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace classlar
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs();
+            kurs1.NameOFCourse = "c++";
+            kurs1.Instructor = "No Name Voman";
+            kurs1.Views = 100;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.NameOFCourse = "c#";
+            kurs2.Instructor = "No Name Man";
+            kurs2.Views = 100;
+        
+            Kurs kurs3 = new Kurs();
+            kurs3.NameOFCourse = "java";
+            kurs3.Instructor = "No Name Voman";
+            kurs3.Views = 100;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.NameOFCourse = "ruby";
+            kurs4.Instructor = "No Name Man";
+            kurs4.Views = 100;
+
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4 };
+            foreach (Kurs kurs in kurslar)
+            {
+                Console.WriteLine(kurs.NameOFCourse + " : " + kurs.Instructor);
+            }
+
+        }
+    }
+
+    class Kurs
+    {
+        public string NameOFCourse{ get; set; }
+        public string Instructor { get; set; }
+        public int Views { get; set; }
+    }
+}
---
+++ consoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace consoleApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] kurslar = new string[]
+                {
+                    "C++", "C#", "JAVA", "Ruby"
+                };
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("for döngüsü sona erdi");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("the end of the code");
+        }
+    }
+}
