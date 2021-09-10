2fa29a9a9b0556ffb017e111fe3b1cb840b4ac33
mevludeortac <ortacmevlude@gmail.com>
2021-09-11 02:04:32 +0300

    Constructors

---
+++ Constructors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Constructors
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Customer customer = new Customer(2, "jay", "z", "los angeles");
+            Console.WriteLine(customer.FirsName);
+
+        }
+    }
+
+    class Customer
+    {
+        //cunstructor
+        public Customer()
+        {
+
+        }
+        //default cunstructor
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirsName = firstName;
+            LastName = lastName;
+            City = city;
+
+        }
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+
+    }
+}
--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 namespace GenericsIntro
 {
-    public class MyList
+    public class MyList<T>
     {
+        T[] items;
+        //ctor ile oluşturuldu
+        //constructor: newlediğimiz anda çalışan blok
         public MyList()
         {
+            items = new T[0];    
         }
+        public void Add(T item)
+        {
+            //yeni bi dizi elemanı ekledikçe eski elemanlar silinir
+            //bu elemanların silinmemesi içim tempArray dizisinde barındırıyoruz
+            //
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            //eski elemanları saklaması için kullandığımız değişkenimizden, eski elemanlarımızı çağırıyoruz
+            for (int i = 0; i < tempArray.Length; i++) //tempArraydeki tüm elemanları tek tek  items'a alıyoruz
+            {
+
+                items[i] = tempArray[i]; //transfer tamamlandı ve artık boş bir elemanımız daha var (yeni dizi elemanı, diğer elemanlar silinmeden eklenmiş oldu)
+            }
+            items[items.Length - 1] = item; // add fonksiyonun parametresi olan item'ı items dizisinin son elemanı olarak atadık.
+
+
+            //yani böyle hayvan gibi kod yazmak yerine List yapısını kullanalım.
+        }
+        
     }
 }
