e433084d79d39d428de8122297c62d7f9e70f195
mevludeortac <ortacmevlude@gmail.com>
2021-09-16 18:56:59 +0300

    Multiple Implementation

---
+++ MultipleImplementation/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace MultipleImplementation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+    }
+
+        interface IWorker
+            {
+                void Work();
+            }
+
+        interface IEat
+            {
+                void Eat();
+            }
+
+        interface ISalary
+            {
+               void GetSalary();
+            }
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    } 
+}
+        
