f66c4176df6b309ad4053780b4633962ba4bf186
mevludeortac <ortacmevlude@gmail.com>
2021-09-16 17:09:11 +0300

    Get and Set Details

---
+++ GetNSetsDetails/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace GetNSetsDetails
+{
+    public class Customer
+    {
+        private string _firstName;
+        public int Id { get; set; }
+        //implementasyon detayının gizlenmesi: encapsulation
+        //property'e herhangi bir özellik eklemek istediğimizde get ve set içine yazabiliriz
+        public string Name {
+            get
+                {
+                return "Mrs. " + _firstName;
+                }
+            set
+                {
+                _firstName = value;
+                }
+        }
+    }
+}
---
+++ GetNSetsDetails/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GetNSetsDetails
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 0003;
+            customer1.Name = "Herbin";
+            Console.WriteLine(customer1.Name);
+        }
+    }
+}
