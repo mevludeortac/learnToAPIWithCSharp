a52124a6ae75b79f686c1b51e55c31f08f4b30da
mevludeortac <ortacmevlude@gmail.com>
2021-09-18 02:03:06 +0300

    difference between inheritance and interface

---
+++ DifferenceBetweenInheritanceAndInterface/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DifferenceBetweenInheritanceAndInterface
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager();
+            customerManager.Add();
+
+            IPersonManager employeeManager = new EmployeeManager();
+            employeeManager.Add();
+        }
+    }
+
+    interface IPersonManager
+    {
+        //uniplemented operation: tamamlanmamış operasyon
+        //just like signature
+        //we need to implement to interface in our class
+        void Add();
+    }
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //adding codes (customer)
+            Console.WriteLine("simulated to adding customer");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            //adding codes (employee)
+            Console.WriteLine("simulated to adding employee");
+        }
+    }
+
+    //inheritance
+    //override and new props are added 
+
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    class Basket : Product
+    {
+        public int BasketId { get; set; }
+        public double Amount { get; set; }
+
+    }
+}
